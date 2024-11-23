d6c195e6b8ced5800fe03620360be1e3e4386b58
Omid Firooz <omid.firoozi2000@gmail.com>
2024-11-23 12:41:15 +0430

    08-The 'account/profile' url has been done

--- account/models.py
+++ account/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account_profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
     github_link = models.URLField(max_length=100, blank=True, null=True)
--- tech_hub/settings.py
+++ tech_hub/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 LOGIN_URL = '/account/login/'
 LOGIN_REDIRECT_URL = '/'
